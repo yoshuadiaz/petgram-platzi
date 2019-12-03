7d42e57e9516e18a2c1f583d85031798252f37a8
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-12-03 16:10:15 -0600

    Add now and global styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './GlobalStyles'
+
+const App = props => (
+  <>
+    <GlobalStyles />
+    <ListOfCategories />
+  </>
+)
+
+export default App
---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+export const List = styled.ul`
+  display: flex;
+  overflow: scroll;
+  width: 100%;
+  padding: 5px;
+`
+
+export const Item = styled.li`
+  padding: 0 .5em;
+`
